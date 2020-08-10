2ffe56637d2dbb04e1fca741ddaa02ec1a0af48e
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-08-11 00:32:51 +0200

    Init with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :events, foreign_key: 'organizer_id', class_name: 'User',  dependent: :destroy
   has_many :attendances, foreign_key: 'attendee_id', class_name: 'User'
   has_many :events, foreign_key: 'attendee_id', class_name: 'User', through: :attendances
