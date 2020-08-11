39d6bca4e07ad21606294b08bf099e1df1443527
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-08-11 20:08:02 +0200

    Update

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def contact_info
+    
+  end
+
+  def credit_card
+
+  end
+
+  def user_secret
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,9 @@ Rails.application.routes.draw do
 
   resources :events 
   
+
+  # Account settings
+  get 'accountsettings/accountsettings', to: 'users#contact_info'
+  get 'accountsettings/creditcard', to: 'users#credit_card'
+  get 'accountsettings/usersecret', to: 'users#user_secret'
 end
